b81dea1c183e8ef28c5f8fdd4d656255f17066de
sv_cheats 1 <35897004+sv-cheats1@users.noreply.github.com>
2018-03-20 15:12:26 +0530

    Create run_contrast.sh

---
+++ Preprocessing/run_contrast.sh
@@ -0,0 +1,21 @@
+# To run, this assumes that you are in the directory with the images
+# unpacked into train/ and test/.  To run, it works best to use GNU
+# parallel as
+#
+#   ls train/*.jpeg test/*.jpeg | parallel ./Preprocessing/run_contrast.sh
+
+out_dir=processed/run-contrast-1
+mkdir -p $out_dir/train
+mkdir -p $out_dir/test
+out=$out_dir/$1
+[ -e $out ] && echo "Skip $1" || echo "$1 -> $out"
+[ -e $out ] || \
+    convert -fuzz 10% -trim +repage -sigmoidal-contrast 5,75% -resize $size -gravity center -background black -extent $size -equalize $1 $out
+
+out_dir=processed/run-contrast-2
+mkdir -p $out_dir/train
+mkdir -p $out_dir/test
+out=$out_dir/$1
+[ -e $out ] && echo "Skip $1" || echo "$1 -> $out"
+[ -e $out ] || \
+convert -fuzz 10% -trim +repage -sigmoidal-contrast 10,50% -resize $size -gravity center -background black -extent $size -equalize $1 $out
