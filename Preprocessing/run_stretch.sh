c885531baec6ffdb8577e9467dd19eeb7c6438f8
sv_cheats 1 <35897004+sv-cheats1@users.noreply.github.com>
2018-03-20 15:08:11 +0530

    Create run_stretch.sh

---
+++ Preprocessing/run_stretch.sh
@@ -0,0 +1,12 @@
+# To run, this assumes that you are in the directory with the images
+# unpacked into train/ and test/.  To run, it works best to use GNU
+# parallel as
+#
+#   ls train/*.jpeg test/*.jpeg | parallel ./prep_image.sh
+out_dir=processed/run-stretch
+mkdir -p $out_dir/train
+mkdir -p $out_dir/test
+out=$out_dir/$1
+[ -e $out ] && echo "Skip $1" || echo "$1 -> $out"
+[ -e $out ] || \
+convert -fuzz 10% -trim +repage -transparent black -contrast-stretch 2x2% -resize $size -gravity center -background black -extent $size -equalize $1 $out
